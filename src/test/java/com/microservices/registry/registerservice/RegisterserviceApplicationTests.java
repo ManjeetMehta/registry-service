b96e8f8624a1c81228df6c962b1d6e6b5203fb0e
Manjeet Mehta <37746654+ManjeetMehta@users.noreply.github.com>
2024-12-25 00:44:35 +0530

    code push

---
+++ src/test/java/com/microservices/registry/registerservice/RegisterserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.registry.registerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
